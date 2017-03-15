7746184a5423a4a59548504ce1e8c7703500833c
Josh Cawthorne <joshcawthorne97@gmail.com>
2017-03-15 01:33:51 +0000

    Added search method using JQuery. Wrapped content in divs. Added stylesheet.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
 var selection = 0;
+var n = 0;
+var text;
 var url = "http://www.netflix.com/browse/genre/"
 
 function setupCategory() {
@@ -7,6 +9,22 @@ function setupCategory() {
     win.focus();
 }
 
-function search() {
+function testSearch() {
+  $('.form-search').on('submit',function(e){return false;});
+  $('.form-search .btn').on('click', function(e){
+      var query = $.trim($(this).prevAll('.search-query').val()).toLowerCase();
+      $('div.cat-contain .bold').each(function(){
+           var $this = $(this);
+           if($this.text().toLowerCase().indexOf(query) === -1)
+               $this.closest('div.cat-contain').fadeOut();
+          else $this.closest('div.cat-contain').fadeIn();
+      });
+  });
+}
+function searchInputDetection() {
+  window.addEventListener('input', function (e) {
 
+    //TODO: Create method that runs the hiding of unrelated items instantly on userinput, as opposed to forcing a user to press enter on their search.
+
+  })
 }
